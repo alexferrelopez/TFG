bf5f040577f69e48cfbffc24c6d770126704864e
Àlex Ferré López <alex.fl@students.salle.url.edu>
2025-08-07 14:10:42 +0200

    Refactor CompassButton styles for improved layout and interaction feedback

--- demo_node_2/src/components/CompassButton.vue
+++ demo_node_2/src/components/CompassButton.vue
@@ -35,15 +35,21 @@ const props = defineProps({
 
 <style scoped>
 .north-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 4rem;
   margin: 10px;
   position: absolute;
   top: 1rem;
   right: 1rem;
   z-index: 2;
-  background: transparent;
+  background: white;
   border: none;
   padding: 0;
   cursor: pointer;
+  border-radius: 50%;
   transition: opacity 0.2s;
 }
 .north-button:disabled {
@@ -51,9 +57,19 @@ const props = defineProps({
   cursor: default;
 }
 .compass-icon {
-  width: 3rem;
-  height: 3rem;
   display: block;
-  transform-origin: center center;
+  width: 50%;
+  height: auto;
+}
+
+.north-button:hover {
+  box-shadow:
+    inset 5px 5px 10px #f5f5f5,
+    inset -5px -5px 10px #fff;
+}
+.north-button:active {
+  box-shadow:
+    inset 5px 5px 10px #d9d9d9,
+    inset -5px -5px 10px #fff;
 }
 </style>
