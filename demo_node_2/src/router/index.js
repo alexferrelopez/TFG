2b635059980dee22c09e4e3628124d7ce0875bf3
Àlex Ferré López <alex.fl@students.salle.url.edu>
2025-08-14 00:18:27 +0200

    refactor: remove Login and Register routes from router configuration

--- demo_node_2/src/router/index.js
+++ demo_node_2/src/router/index.js
@@ -1,13 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Login from '@/pages/Login.vue'
-import Register from '@/pages/Register.vue'
 import MainPage from '@/pages/MainPage.vue'
 
 const routes = [
-  { path: '/', component: MainPage },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register }
+  { path: '/', component: MainPage }
 ]
 
 const router = createRouter({
